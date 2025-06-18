1c4dbdf116836468b324d01cfeb836dd0c60229c
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 17:35:07 -0400

    Delete docusaurus images and homepage features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -37,7 +36,7 @@ export default function Home(): ReactNode {
       description="Three days. One game. As many devs as we can possibly get.">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        
       </main>
     </Layout>
   );
