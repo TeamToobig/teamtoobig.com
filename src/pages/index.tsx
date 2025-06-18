aa337bcc44f6e2e58bf284bc3fbbcad42556be0e
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 18:42:23 -0400

    Remove colored banner from homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
@@ -10,12 +9,12 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
+    <header className={styles.heroContent}>
+      <div className={styles.heroContainer}>
+        <Heading as="h1" className={styles.heroTitle}>
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -35,7 +34,9 @@ export default function Home(): ReactNode {
       title={`Team Toobig`}
       description="Three days. One game. As many devs as we can possibly get.">
       <div className={styles.background} />
-      <HomepageHeader />
+      <div className={styles.heroWrapper}>
+        <HomepageHeader />
+      </div>
       <main>
         
       </main>
