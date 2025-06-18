fea24ea42358052fd1de5b1dda21d5b30905226a
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 01:22:43 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Team Toobig',
+  tagline: 'The biggest game jam team ever',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://teamtoobig.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TeamToobig', // Usually your GitHub org/user name.
+  projectName: 'teamtoobig.com', // Usually your repo name.
+
+  trailingSlash: false, // The docs strongly encouraged me to add this explicitly
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
