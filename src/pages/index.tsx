134d74df8244493dde5407eb8a025dc9171966c4
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 18:14:21 -0400

    Update homepage button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/apply">
+            Apply to join 🚀
           </Link>
         </div>
       </div>
