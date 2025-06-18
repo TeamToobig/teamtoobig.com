50e9430e57a65df8a6068eb1437e16c19a27cfe8
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 13:30:58 -0400

    Remove github link in navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,11 +81,6 @@ const config: Config = {
           label: 'Tutorial',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
