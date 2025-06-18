f6fc9a65bc996eee3562805f36532b1620804841
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-18 15:43:09 -0400

    Update social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     navbar: {
       title: 'Team Toobig',
       logo: {
