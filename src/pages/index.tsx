95947552f7e6417ac0a736cfc4e6c052d59a1e4a
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-22 21:15:24 -0400

    Simplify homepage
    Remove heroContent, remove InfoBox function in favor of putting everything in the Home function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,10 @@
 import type {ReactNode} from 'react';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import Terry from '@site/src/components/FloatingTerry';
 
 import styles from './index.module.css';
-
-function InfoBox() {
-  return (
-    <header className={styles.heroContent}>
-      <div className={styles.heroContainer}>
-        <Heading as="h1" className={styles.heroTitle}>Team Toobig</Heading>
-        <p className={styles.heroSubtitle}>We're assembling the biggest game jam team in history.</p>
-        <p className={styles.heroSubtitle}>Three days. One game. As many devs as we can possibly get.</p>
-        <div className={styles.buttons}>
-          <Link
-            className={`button button--secondary button--lg ${styles.wideButton} ${styles.learnMoreButton}`}
-            to="/about">
-            Learn more
-          </Link>
-          <Link
-            className={`button button--secondary button--lg ${styles.wideButton} ${styles.applyButton}`}
-            to="/apply">
-            Apply to join 🚀
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): ReactNode {
   return (
@@ -43,7 +18,23 @@ export default function Home(): ReactNode {
           <Terry/>
         </div>
         <div className={styles.infoContainer}>
-          <InfoBox />
+          <header className={styles.heroContainer}>
+            <Heading as="h1" className={styles.heroTitle}>Team Toobig</Heading>
+            <p className={styles.heroSubtitle}>We're assembling the biggest game jam team in history.</p>
+            <p className={styles.heroSubtitle}>Three days. One game. As many devs as we can possibly get.</p>
+            <div className={styles.buttons}>
+              <Link
+                className={`button button--secondary button--lg ${styles.wideButton} ${styles.learnMoreButton}`}
+                to="/about">
+                Learn more
+              </Link>
+              <Link
+                className={`button button--secondary button--lg ${styles.wideButton} ${styles.applyButton}`}
+                to="/apply">
+                Apply to join 🚀
+              </Link>
+            </div>
+          </header>
         </div>
       </div>
     </Layout>
