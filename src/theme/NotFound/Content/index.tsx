a6f676a84dc79d50be2ce2edae3224e8370c3f30
Jimmy Cushnie <git@jimmycushnie.com>
2025-06-23 15:40:12 -0400

    Add custom 404 page
    Just wanted to inject a bit of personality into it.
    
    For reference, this was done with `npm run swizzle @docusaurus/theme-classic NotFound`. https://docusaurus.io/docs/swizzling#swizzling-theme-components

---
+++ src/theme/NotFound/Content/index.tsx
@@ -0,0 +1,37 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import type {Props} from '@theme/NotFound/Content';
+import Heading from '@theme/Heading';
+
+export default function NotFoundContent({className}: Props): ReactNode {
+  return (
+    <main className={clsx('container margin-vert--xl', className)}>
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <Heading as="h1" className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </Heading>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page">
+              Oopsie whoopsie! That's a 404, I'm afraid.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              Please contact the person who linked you here and let them know their link is broken.
+            </Translate>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
